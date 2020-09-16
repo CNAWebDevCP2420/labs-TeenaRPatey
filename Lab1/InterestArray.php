275bac47ca641230b66249874cac4b1c83f677e3
TeenaRPatey <64781812+TeenaRPatey@users.noreply.github.com>
2020-09-16 20:35:06 -0230

    Add files via upload

---
+++ Lab1/InterestArray.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<head>
+<title>Interest Array</title>
+</head>
+<body>
+<p>
+<?php 
+$InterestRate = array(.0725, .0750, .0775, .0800, .0825, .0850, .0875);
+echo($InterestRate);
+?>
+</p>
+</body>
+</html>
