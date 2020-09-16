70245c8d7bdc32259adf29a04bd3ae3655c76eaf
TeenaRPatey <64781812+TeenaRPatey@users.noreply.github.com>
2020-09-16 21:20:22 -0230

    Create SingleFamilyHome.php

---
+++ Lab1/SingleFamilyHome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<head>
+<title>Single Family Home</title>
+</head>
+<body>
+<p>
+<?php 
+$SingleFamilyHome = 399500;
+$SingleFamilyHome_Display =
+    number_format (@$SingleFamilyHome, 2);
+echo "<p>The current median price of a single family home in Pleasanton, CA is
+    $$SingleFamilyHome_Display.</p>";   
+?>
+</p>
+</body>
+</html>
