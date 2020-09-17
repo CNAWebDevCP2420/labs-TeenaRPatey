eca1f77d3c9441da77c427e961cbcb110698ee42
TeenaRPatey <64781812+TeenaRPatey@users.noreply.github.com>
2020-09-17 20:03:03 -0230

    coast city computers

---
+++ Lab1/inc_header.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<table width="100%" style="border: 0">
+<tr><td><h1>Coast City Computers</h1></td>
+<td style = "text-align: right"><strong>Buy Online or Call 1-800-555-1212</strong></td></tr></table><hr />
